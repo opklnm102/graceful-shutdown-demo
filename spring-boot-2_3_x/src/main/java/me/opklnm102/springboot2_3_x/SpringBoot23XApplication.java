35a1ca1d6a04531bf737102be376489621b4c4dc
Hue Kim <opklnm102@gmail.com>
2020-07-01 11:37:07 +0900

    initial commit

---
+++ spring-boot-2_3_x/src/main/java/me/opklnm102/springboot2_3_x/SpringBoot23XApplication.java
@@ -0,0 +1,13 @@
+package me.opklnm102.springboot2_3_x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot23XApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot23XApplication.class, args);
+    }
+
+}
---
+++ spring-boot-2_3_x/src/test/java/me/opklnm102/springboot2_3_x/SpringBoot23XApplicationTests.java
@@ -0,0 +1,13 @@
+package me.opklnm102.springboot2_3_x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot23XApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
